ec358bf885f58fd6c38063c894f0bfa69a5bb58a
Joshua Trujillo <joshmtrujillo@gmail.com>
2023-05-29 11:08:21 -0400

    Setting up config path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const app = require("./app");
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "/home/josh/environment-variables/config.env" });
 const port = process.env.PORT;
 const env = process.env.NODE_ENV;
 
